fa3eb3dcf524ba3fca46f407d6c5c2de60294863
mr__lordi <mr__lordi>
2016-05-26 13:32:48 +0300

    third

--- tcpServer/client.js
+++ tcpServer/client.js
@@ -3,16 +3,20 @@ global.api = api;
 api.net = require('net');
 
 var socket = new api.net.Socket();
-var user;
+var result;
+var task;
 
 socket.connect({
-  port: 2000,
-  host: '127.0.0.1',
-}, function() {
-  socket.write('Hello from client');
-  socket.on('data', function(data) {
-    user = JSON.parse(data);
-    console.log('Data received (by client): ' + data);
-    console.log('Age of ' + user.name + ' is ' + user.age);
-  });
+    port: 2000,
+    host: '127.0.0.1'
+}, function () {
+    socket.on('data', function (data) {
+        task = JSON.parse(data);
+        console.log('Data received (by client): ' + data);
+        result = task.map(function (elem) {
+            return elem * 2;
+        });
+        socket.write(JSON.stringify(result));
+        socket.destroy();
+    });
 });
--- tcpServer/server.js
+++ tcpServer/server.js
@@ -1,13 +1,44 @@
-var api = {};
-global.api = api;
-api.net = require('net');
+var api = {}; 
+global.api = api; 
+api.net = require('net'); 
+api.os = require('os'); 
 
-var user = { name: 'Marcus Aurelius', age: 1895 };
+var task = [2, 17, 3, 2, 5, 7, 15, 22, 1, 14, 15, 9, 0, 11]; 
+var cpuNumber = api.os.cpus().length; 
+var parts = []; 
+var clients = []; 
+var result = []; 
+var part = task.length / cpuNumber; 
+var server = api.net.createServer(function(socket) { 
 
-var server = api.net.createServer(function(socket) {
-  socket.write(JSON.stringify(user));
-  console.log('Connected: ' + socket.localAddress);
-  socket.on('data', function(data) {
-    console.log('Data received (by server): ' + data);
-  });
+	socket.id = clients.length; 
+	var count = clients.length; 
+	
+	var startIndex = 0; 
+	clients.push(socket); 
+	if (clients.length == cpuNumber) { 
+		for (var i = 0; i < clients.length; i++) { 
+			clients[i].write(JSON.stringify(task.slice(startIndex, startIndex + part)));  
+			startIndex = part * (i+1); 
+		} 
+	} 
+
+	socket.on('data', function(data) { 
+		console.log('Result recieved by server:' + data); 
+		var res = {}; 
+		res.data = JSON.parse(data); 
+		res.id = socket.id; 
+		parts[count] = res; 
+		result = [];
+		parts.sort(function(a, b) { 
+			if (a.id < b.id) {
+				return -1; 
+			} else { 
+				return 1;
+			} 
+		}).forEach(function(res) { 
+			result = result.concat(res.data); 
+		}); 
+		console.log("General result is: " + result); 
+	}); 
 }).listen(2000);
